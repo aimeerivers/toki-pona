170404c27709eab186453592566894ce19402aeb
aimee rivers <aimee@aimeerivers.com>
2024-01-31 23:18:03 +0100

    style(chapter): add a new line

--- js/chapter.js
+++ js/chapter.js
@@ -6,6 +6,7 @@ const chapter = data.chapters[chapterNumber - 1];
 const lessonsContainer = document.getElementById('lessons-container');
 const heading1 = document.getElementsByTagName('h1')[0];
 const heading2 = document.getElementsByTagName('h2')[0];
+
 heading1.textContent = `Chapter ${chapterNumber}`;
 heading2.textContent = chapter.chapterName;
 
