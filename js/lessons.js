ec0449ccf56388166b6a476ceca506008ff1be1a
aimee rivers <aimee@aimeerivers.com>
2024-01-31 21:55:34 +0100

    feat(lessons): load lesson data and randomize challenges

---
+++ js/lessons.js
@@ -0,0 +1,77 @@
+const data = {
+  "chapters": [
+    {
+      "chapterNumber": 1,
+      "chapterName": "Basic sentences",
+      "vocabulary": [
+        {
+          "tokiPona": "mi",
+          "english": "I, me, we"
+        },
+        {
+          "tokiPona": "sina",
+          "english": "you"
+        },
+        {
+          "tokiPona": "moku",
+          "english": "food, eat, meal"
+        },
+        {
+          "tokiPona": "pona",
+          "english": "good, simple, positive, nice, correct, right"
+        },
+        {
+          "tokiPona": "telo",
+          "english": "water, liquid, juice, sauce"
+        },
+        {
+          "tokiPona": "suno",
+          "english": "sun, light, shine, bright"
+        },
+        {
+          "tokiPona": "ona",
+          "english": "she, he, it, they"
+        },
+        {
+          "tokiPona": "suli",
+          "english": "big, tall, long, adult, important"
+        },
+        {
+          "tokiPona": "jan",
+          "english": "person, people, human, somebody, anybody"
+        }
+      ],
+      "lessons": [
+        {
+          "lessonNumber": 1,
+          "challenges": [
+            {
+              "tokiPona": "moku li pona",
+              "english": "the food is good"
+            },
+            {
+              "tokiPona": "mi moku",
+              "english": "I eat"
+            },
+            {
+              "tokiPona": "telo li pona",
+              "english": "water is good"
+            },
+            {
+              "tokiPona": "suno li suno",
+              "english": "the sun is shining"
+            },
+            {
+              "tokiPona": "ona li moku",
+              "english": "they eat"
+            },
+            {
+              "tokiPona": "sina pona",
+              "english": "you are good"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+let sitelenPona = document.getElementById('sitelen-pona');
+let tokipona = document.getElementById('toki-pona');
+let english = document.getElementById('english');
+
+const chapter = data.chapters.find(chapter => chapter.chapterNumber === 1);
+const vocabulary = chapter.vocabulary;
+const lesson = chapter.lessons.find(lesson => lesson.lessonNumber === 1);
+const challenges = lesson.challenges;
+
+const randomChallenge = challenges[Math.floor(Math.random() * challenges.length)];
+displayChallenge(randomChallenge);
+
+function displayChallenge(challenge) {
+  sitelenPona.textContent = challenge.tokiPona;
+  tokipona.textContent = challenge.tokiPona;
+  english.textContent = challenge.english;
+}
+
 function flipCard() {
   const card = document.getElementById('card');
   card.classList.toggle('flipped');
