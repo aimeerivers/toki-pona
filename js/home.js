f95d1403749c5192147714b6596874a731b0dc00
aimee rivers <aimee@aimeerivers.com>
2024-02-01 20:22:28 +0100

    style(home): remove word "Chapter" from buttons

--- js/home.js
+++ js/home.js
@@ -4,7 +4,7 @@ const chaptersContainer = document.getElementById('chapters-container');
 data.chapters.forEach((chapter, index) => {
   const chapterLink = document.createElement('a');
   chapterLink.href = `chapter.html?chapter=${index + 1}`;
-  chapterLink.textContent = `Chapter ${index + 1}: ${chapter.chapterName}`;
+  chapterLink.textContent = `${index + 1}: ${chapter.chapterName}`;
   chapterLink.className = 'w-64 mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block text-center';
   chaptersContainer.appendChild(chapterLink);
 });
