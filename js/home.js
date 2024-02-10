8144738a9de6c68a6f7c63f3d9b57d6b80a78958
aimee rivers <aimee@aimeerivers.com>
2024-02-10 18:55:39 +0100

    feat(home): display chapters as cards in a grid

--- js/home.js
+++ js/home.js
@@ -2,9 +2,22 @@
 const chaptersContainer = document.getElementById('chapters-container');
 
 data.chapters.forEach((chapter, index) => {
+  const card = document.createElement('div');
+  card.className = 'bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded block text-center';
+
   const chapterLink = document.createElement('a');
   chapterLink.href = `chapter.html?chapter=${index + 1}`;
-  chapterLink.textContent = chapter.chapterName;
-  chapterLink.className = 'w-64 mt-4 bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded block text-left';
-  chaptersContainer.appendChild(chapterLink);
+
+  const chapterEmoji = document.createElement('span');
+  chapterEmoji.textContent = chapter.chapterEmoji;
+  chapterEmoji.className = 'text-4xl block';
+
+  const chapterName = document.createElement('span');
+  chapterName.textContent = chapter.chapterName;
+  chapterName.className = 'block';
+
+  chapterLink.appendChild(chapterEmoji);
+  chapterLink.appendChild(chapterName);
+  card.appendChild(chapterLink);
+  chaptersContainer.appendChild(card);
 });
